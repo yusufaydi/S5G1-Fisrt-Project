eb28a31f0cb6e1e77b597451bd17ff5c312523b4
yusufaydi <bm.yusufaydin@gmail.com>
2024-01-14 18:34:23 +0300

    updated

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 const siteContent = {
   // BU NESNEYİ DEĞİŞTİRMEYİN
-  title: 'Workintech',
-  logo: 'https://i.ibb.co/ZmRv388/logo.png',
+  title: "Workintech",
+  logo: "https://i.ibb.co/ZmRv388/logo.png",
   links: [
     {
-      href: 'programlarimiz.html',
-      text: 'Programlarımız',
+      href: "programlarimiz.html",
+      text: "Programlarımız",
     },
     {
-      href: 'blog.html',
-      text: 'Blog',
+      href: "blog.html",
+      text: "Blog",
     },
     {
-      href: 'sorulariniz.html',
-      text: 'Sorularınız',
+      href: "sorulariniz.html",
+      text: "Sorularınız",
     },
     {
-      href: 'hakkimizda.html',
-      text: 'Hakkımızda',
+      href: "hakkimizda.html",
+      text: "Hakkımızda",
     },
   ],
 };
 
 /* Kodlar Buradan aşağıya */
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   navModel();
 });
 
 function navModel() {
-  let navUrl = document.getElementById('logo-img');
-  navUrl.setAttribute('src', siteContent.logo);
+  let navUrl = document.getElementById("logo-img");
+  navUrl.setAttribute("src", siteContent.logo);
   for (let i = 0; i <= 4; i++) {
-    let aHref = document.getElementsByTagName('a')[i];
-    aHref.setAttribute('href', siteContent.links[i].href);
+    let aHref = document.getElementsByTagName("a")[i];
+    aHref.setAttribute("href", siteContent.links[i].href);
     aHref.textContent = siteContent.links[i].text;
   }
 }
 
-let title = document.getElementsByTagName('title')[0];
+let title = document.getElementsByTagName("title")[0];
 
 title.textContent = siteContent.title;
